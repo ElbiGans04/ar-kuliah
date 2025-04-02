032f39f1a245ccf2d1d5b6ef69834c462c21d32d
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2025-04-02 17:42:36 +0700

    feat: membuat halaman home

--- views/home.tsx
+++ views/home.tsx
@@ -1,25 +1,118 @@
-import {useNavigation} from '@react-navigation/native';
-import React, {Button, Text, View} from 'react-native';
+// import {useNavigation} from '@react-navigation/native';
+import React, {
+  // Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+const styles = StyleSheet.create({
+  container: {height: '100%', flex: 1},
+  scrollViewContainer: {height: '100%', flex: 1},
+  containerHead: {
+    paddingTop: 50,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 50,
+    width: '100%',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerHeadTitle: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  containerBody: {
+    backgroundColor: '#D9D9D9',
+    marginTop: -40,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    paddingTop: 40,
+    paddingBottom: 40,
+    paddingHorizontal: 20,
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    rowGap: 25,
+    columnGap: 15,
+    justifyContent: 'center',
+    flexShrink: 1
+
+  },
+  button: {
+    backgroundColor: 'white',
+    padding: 20,
+    flex: 1,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  buttonText: {
+    fontSize: 16,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+});
 
 function Home() {
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
 
   return (
-    <View>
-      <Text>This Is Home Page</Text>
-      <Button
-        onPress={() => {
-          navigation.navigate('About' as never);
-        }}
-        title="Go To About"
-      />
-       <Button
-        onPress={() => {
-          navigation.navigate('Ar' as never);
-        }}
-        title="Go To Ar"
-      />
-    </View>
+    <ScrollView style={styles.scrollViewContainer}>
+      <View style={styles.container}>
+        <LinearGradient colors={['#513CCA', '#281E64']}>
+          <View style={styles.containerHead}>
+            <Text style={styles.containerHeadTitle}>Judul Aplikasi</Text>
+            <Image source={require('../assets/icons/Picture.png')}></Image>
+          </View>
+        </LinearGradient>
+        <View style={styles.containerBody}>
+          <TouchableOpacity activeOpacity={0.7}>
+            <View style={styles.button}>
+              <Image source={require('../assets/icons/Camera.png')} />
+              <Text style={styles.buttonText}>Camera</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.7}>
+            <View style={styles.button}>
+              <Image source={require('../assets/icons/Test.png')} />
+              <Text style={styles.buttonText}>Quiz</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.7}>
+            <View style={styles.button}>
+              <Image source={require('../assets/icons/Index.png')} />
+              <Text style={styles.buttonText}>Panduan</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.7}>
+            <View style={styles.button}>
+              <Image source={require('../assets/icons/Info.png')} />
+              <Text style={styles.buttonText}>Tentang</Text>
+            </View>
+          </TouchableOpacity>
+          {/* <Button
+            onPress={() => {
+              navigation.navigate('About' as never);
+            }}
+            title="Go To About"
+          />
+          <Button
+            onPress={() => {
+              navigation.navigate('Ar' as never);
+            }}
+            title="Go To Ar"
+          /> */}
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
