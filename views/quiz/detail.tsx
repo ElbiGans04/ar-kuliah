54c8ffe4663c2c6523d80c8d4b64ee35ca2010c0
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2025-04-22 00:47:54 +0700

    feat: membuat logic modal halaman quiz detail

--- views/quiz/detail.tsx
+++ views/quiz/detail.tsx
@@ -1,6 +1,15 @@
+import {
+  EventListenerCallback,
+  EventMapCore,
+  StackNavigationState,
+} from '@react-navigation/native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import BackgroundWithSectionLayout from 'layouts/backgroundWithSection';
-import React, {useState} from 'react';
+import {NativeStackNavigationEventMap} from 'node_modules/@react-navigation/native-stack/lib/typescript/commonjs/src';
+import React, {useEffect, useState} from 'react';
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import ReactNativeModal from 'react-native-modal';
+import {ScreenType} from 'routes';
 
 const styles = StyleSheet.create({
   containerText: {
@@ -34,6 +43,48 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#0A3180',
   },
+  modalContainer: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 10,
+    gap: 16,
+  },
+  modalHeading: {
+    color: '#0A3180',
+    fontSize: 20,
+    fontWeight: 700,
+  },
+  textBold: {
+    fontWeight: 'bold',
+  },
+  item: {
+    backgroundColor: '#CFCDCD',
+    borderRadius: 10,
+    padding: 10,
+    width: '45%',
+  },
+  itemText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#0A3180',
+    textAlignVertical: 'center',
+  },
+  item2: {
+    backgroundColor: '#0A3180',
+    borderRadius: 10,
+    padding: 10,
+    width: '45%',
+  },
+  itemText2: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    textAlignVertical: 'center',
+  },
+  modalContainerAction: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 });
 
 const answer = [
@@ -57,8 +108,40 @@ const answer = [
   },
 ];
 
-export default function QuizDetailView() {
+export default function QuizDetailView({
+  navigation,
+}: NativeStackScreenProps<ScreenType, 'MateriDetail'>) {
   const [next, setNext] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [showModal2, setShowModal2] = useState(false);
+  const [allowBack, setAllowBack] = useState(false);
+
+  // Register Event
+  useEffect(() => {
+    const handler: EventListenerCallback<
+      NativeStackNavigationEventMap &
+        EventMapCore<StackNavigationState<ScreenType>>,
+      'beforeRemove',
+      true
+    > = event => {
+      console.log(`get called`);
+      if (!allowBack) {
+        event.preventDefault();
+        setShowModal(true);
+      }
+    };
+    navigation.addListener('beforeRemove', handler);
+
+    return () => {
+      navigation.removeListener('beforeRemove', handler);
+    };
+  }, [navigation, setShowModal, allowBack]);
+
+  useEffect(() => {
+    if (allowBack) {
+      navigation.goBack();
+    }
+  }, [allowBack, navigation]);
 
   return (
     <BackgroundWithSectionLayout>
@@ -66,6 +149,68 @@ export default function QuizDetailView() {
         <View style={styles.containerHeader}>
           <Text style={styles.textInfo}>Pertanyaan {next + 1} dari 2</Text>
           <Text style={styles.textHeader}>{answer[next].title}</Text>
+        </View>
+
+        <View>
+          <ReactNativeModal isVisible={showModal}>
+            <View style={styles.modalContainer}>
+              <Text style={styles.modalHeading}>Peringatan</Text>
+              <Text>
+                Anda yakin ingin kembali ? Anda akan{' '}
+                <Text style={styles.textBold}>kehilangan semua perubahan</Text>{' '}
+                yang dilakukan.
+              </Text>
+
+              {/* Button Action */}
+              <View style={styles.modalContainerAction}>
+                <TouchableOpacity
+                  onPress={() => {
+                    setAllowBack(true);
+                  }}
+                  style={styles.item}>
+                  <View>
+                    <Text style={styles.itemText}>Lanjutkan</Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => {
+                    setShowModal(false);
+                  }}
+                  style={styles.item2}>
+                  <View>
+                    <Text style={styles.itemText2}>Batalkan</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </ReactNativeModal>
+        </View>
+
+        {/* Modal 2 */}
+        <View>
+          <ReactNativeModal isVisible={showModal2}>
+            <View style={styles.modalContainer}>
+              <Text style={styles.modalHeading}>Selamat</Text>
+              <Text>
+                Anda telah menyelesaikan quiz. Anda mendapatkan point{' '}
+                <Text style={styles.textBold}>sebesar 100/100</Text>
+              </Text>
+
+              {/* Button Action */}
+              <View style={styles.modalContainerAction}>
+                <TouchableOpacity
+                  onPress={() => {
+                    setShowModal2(false);
+                    setAllowBack(true);
+                  }}
+                  style={styles.item}>
+                  <View>
+                    <Text style={styles.itemText}>Lanjutkan</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </ReactNativeModal>
         </View>
 
         <Image
@@ -79,7 +224,14 @@ export default function QuizDetailView() {
               {answer[next].answer.map((val, index) => (
                 <TouchableOpacity
                   key={`${val}-${index}`}
-                  onPress={() => setNext(1)}>
+                  onPress={() => {
+                    if (next !== 1) {
+                      setNext(1);
+                      return;
+                    }
+
+                    setShowModal2(true);
+                  }}>
                   <View style={styles.button}>
                     <Text style={styles.buttonText}>{val}</Text>
                   </View>
