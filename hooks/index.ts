8df1fc8bfba814755167025daa764f6da08b5552
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2025-05-01 20:45:56 +0700

    refactor: ganti relative path ke absolute path pada hooks/index

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,4 +1,4 @@
-import appDetail from '../constants/app-detail';
+import appDetail from 'constants/app-detail';
 import {createContext} from 'react';
 
 export const AppDetailContext = createContext(appDetail);
