30f10923d2bbbbbefa17c809357e46d44595b417
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2025-05-01 20:29:51 +0700

    feat: memperbaharui nilai scale up & scale down model pada AR Page
    memperbaharui nilai scale up & scale down agar setiap menekan
    tombol akan mengurangi / menambahkan 10% dari nilai. dan
    menghapus komentar di bagian bawah file

--- views/ar.tsx
+++ views/ar.tsx
@@ -10,9 +10,13 @@ import {
   ViroSpotLight,
   ViroTrackingStateConstants,
 } from '@reactvision/react-viro';
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Button, StyleSheet, View} from 'react-native';
 import {ScreenType} from 'routes';
+
+function get10Percent(value: number): number {
+  return (value * 10) / 100;
+}
 
 const materi = {
   kucing: {
@@ -81,12 +85,6 @@ function Ar(props: any) {
       });
     }
   }
-
-  useEffect(() => {
-    console.log(
-      `Rotate Y : ${rotateY}. Rotate X : ${rotateX}. Scale: ${scale}`,
-    );
-  }, [scale, rotateX, rotateY]);
 
   return (
     <ViroARScene onTrackingUpdated={onTrackingUpdated}>
@@ -196,7 +194,7 @@ export default function ArView({
           <Button
             onPress={() => {
               setScale(state => {
-                const st = state - 0.1;
+                const st = state - get10Percent(state);
                 return st <= 0 ? 0.1 : st;
               });
             }}
@@ -212,7 +210,7 @@ export default function ArView({
           />
           <Button
             onPress={() => {
-              setScale(state => state + 0.1);
+              setScale(state => state + get10Percent(state));
             }}
             title="Scale Up"
           />
@@ -286,127 +284,3 @@ var styles = StyleSheet.create({
     textAlign: 'center',
   },
 });
-
-/*
-
-  Template Original React Native 0.76
-
-*/
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  */
-
-// import React from 'react';
-// import type {PropsWithChildren} from 'react';
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
-
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
-
-// function Section({children, title}: SectionProps): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// }
-
-// function App(): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar
-//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-//         backgroundColor={backgroundStyle.backgroundColor}
-//       />
-//       <ScrollView
-//         contentInsetAdjustmentBehavior="automatic"
-//         style={backgroundStyle}>
-//         <Header />
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//           }}>
-//           <Section title="Step One">
-//             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-//             screen and then come back to see your edits.
-//           </Section>
-//           <Section title="See Your Changes">
-//             <ReloadInstructions />
-//           </Section>
-//           <Section title="Debug">
-//             <DebugInstructions />
-//           </Section>
-//           <Section title="Learn More">
-//             Read the docs to discover what to do next:
-//           </Section>
-//           <LearnMoreLinks />
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
-
-// export default App;
